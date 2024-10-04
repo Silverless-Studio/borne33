4e1d22da8735fc7564f06af51ac5fd5a87d9a205
Andy Paddock <appaddock@gmail.com>
2024-10-04 09:49:54 +0100

    age gate

--- wp-content/themes/silverless/template-parts/age-gate.php
+++ wp-content/themes/silverless/template-parts/age-gate.php
@@ -1,5 +1,7 @@
-<div id="age-gate" class="age-gate">
-    <div class="age-gate-content">
+<div id="age-gate" class="age-gate container">
+    <div class="background--image-main"><img src="<?php echo get_template_directory_uri(); ?>/inc/img/bg.avif"
+        alt="Bottle Image"></div>
+    <div class="age-gate-content row col-6">
         <div class="logo"><?php get_template_part('inc/img/logo-main'); ?></div>
          <div class="underscores underscores--primary"></div>
         <h2 class="heading-3">The new spirit of vodka</h2>
