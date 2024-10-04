243d24d4aa608cc1028f88712946dc9bb1acd1d9
Andy Paddock <appaddock@gmail.com>
2024-10-04 09:26:03 +0100

    age gate

--- wp-content/themes/silverless/template-parts/age-gate.php
+++ wp-content/themes/silverless/template-parts/age-gate.php
@@ -1,6 +1,7 @@
 <div id="age-gate" class="age-gate">
     <div class="age-gate-content">
         <div class="logo"><?php get_template_part('inc/img/logo-main'); ?></div>
+         <div class="underscores underscores--primary"></div>
         <h2 class="heading-3">The new spirit of vodka</h2>
         <p class="age-intro">You must be 18 years or older to view this site.</p>
         <button id="yes-btn">Enter<?php get_template_part('inc/img/arrow'); ?></button>
