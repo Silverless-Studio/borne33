a03666f7ba5fd0bf1dc59be3a2fb4fa16a5669b1
Andy Paddock <appaddock@gmail.com>
2024-10-07 11:34:21 +0100

    checkout

--- wp-content/themes/silverless/index.php
+++ wp-content/themes/silverless/index.php
@@ -7,6 +7,7 @@ get_header();
 
 <section class="container main-container">
 	<div class="row col-10">
+				<h2 class="heading-4 heading-alt"><?php the_title();?></h2>
 <?php the_content();?>
 	</div>
 </section>
--- wp-content/themes/silverless/woocommerce.php
+++ wp-content/themes/silverless/woocommerce.php
@@ -7,6 +7,7 @@ get_header();
 
 <section class="container shop-container">
 	<div class="row">
+
 <?php woocommerce_content(); ?>
 	</div>
 </section>
