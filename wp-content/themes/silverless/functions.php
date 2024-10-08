3846e92caaecad76f2481009cd39d58f017ba688
Andy Paddock <appaddock@gmail.com>
2024-10-08 13:31:00 +0100

    version number

--- wp-content/themes/silverless/functions.php
+++ wp-content/themes/silverless/functions.php
@@ -377,7 +377,7 @@ function theme_gsap_script(){
     // ScrollTrigger - with gsap.js passed as a dependency
     wp_enqueue_script( 'gsap-st', 'https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollTrigger.min.js', array('gsap-js'), false, true );
     // Your animation code file - with gsap.js passed as a dependency
-    wp_enqueue_script( 'gsap-js2', get_template_directory_uri() . '/inc/js/app.js', array('gsap-js'), false, true );
+    wp_enqueue_script('gsap-js2', get_template_directory_uri() . '/inc/js/app.js', array('gsap-js'), filemtime( get_template_directory() . '/inc/js/app.js' ), true);
 }
 
 add_action( 'wp_enqueue_scripts', 'theme_gsap_script' );
