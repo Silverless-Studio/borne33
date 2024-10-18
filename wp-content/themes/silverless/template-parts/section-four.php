ed42af21a54b9b3c9885f0b745adb01e6e72b101
ashsilverless <ash@silverless.co.uk>
2024-10-18 08:47:38 +0100

    initial

--- wp-content/themes/silverless/template-parts/section-four.php
+++ wp-content/themes/silverless/template-parts/section-four.php
@@ -1,20 +1,20 @@
 <section class="section--four container relative">
     <div class="row col-10 col-half">
-        <div class="flask"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/inc/img/flask.avif" /></div>
+        <div class="flask"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/inc/img/flask.avif" /></div>
         <div class="image--text">
-            <h2 class="heading-3">
-                <?php echo esc_html( get_field('image_text') ); ?>
+            <h2 class="heading-2">
+                <?php echo esc_html(get_field('image_text')); ?>
             </h2>
             <div class="background--image">
 
-               
+
                 <div class="spin-me"><img
-                        src="<?php echo esc_url( get_template_directory_uri() ); ?>/inc/img/wheel.avif" /></div>
-                         <div class="spin-me"><?php get_template_part('inc/img/runes'); ?></div>
+                        src="<?php echo esc_url(get_template_directory_uri()); ?>/inc/img/wheel.avif" /></div>
+                <div class="spin-me"><?php get_template_part('inc/img/runes'); ?></div>
             </div>
         </div>
         <div class="main-description fm-right">
-            <h2 class="heading-4 heading-alt font--color-primary"><?php echo esc_html( get_field('four_title') ); ?>
+            <h2 class="heading-4 heading-alt font--color-primary"><?php echo esc_html(get_field('four_title')); ?>
             </h2>
             <?php the_field('four_description'); ?>
         </div>
